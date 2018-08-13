3a199878efd0c4cf18874937bc64b6eb1d2f78b6
jegalchoi <thefreshbegal@gmail.com>
2018-08-13 15:02:09 -0700

    Add javascript_exercises.html and javascript_exercises.js

---
+++ javascript_exercises.js
@@ -0,0 +1,20 @@
+//1.
+console.log(23 + 97);
+
+//2.
+var x = 1 + 2 + 3 + 4 + 5 + 6;
+
+//3.
+console.log((4 + 6 + 9) / 77);
+
+//4.
+let a = 10;
+console.log(a);
+console.log(9 * a);
+let b = 7 * a;
+console.log(b);
+
+//5.
+const MAX = 57;
+let actual = MAX - 13;
+let percentage = actual / MAX;
